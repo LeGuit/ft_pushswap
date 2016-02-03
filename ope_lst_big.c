a22e5905911a950e7a71b2365200f85a65b7cf8f
Guillaume WOODWARD <gwoodwar@e1r1p10.42.fr>
2016-02-03 15:22:13 +0100

    save

--- ope_lst_big.c
+++ ope_lst_big.c
@@ -6,7 +6,7 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/02 14:54:50 by gwoodwar          #+#    #+#             */
-/*   Updated: 2016/02/02 18:13:37 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2016/02/03 14:58:24 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,22 @@ void			rrx(t_info *info, char *ope)
 			C_NODE(t_node, info->headb.next)->color = 4;
 			C_NODE(t_node, info->headb.prev)->color = 4;
 		}
+	}
+	info->nbope++;
+}
+
+void			rrr(t_info *info)
+{
+	dlst_rrotate(&info->heada);
+	dlst_rrotate(&info->headb);
+	ft_putstr("rrr ");
+	if (GET(info->opt, OPT_C))
+	{
+		unset_color(&info->heada, &info->headb);
+		C_NODE(t_node, info->heada.next)->color = 4;
+		C_NODE(t_node, info->heada.prev)->color = 4;
+		C_NODE(t_node, info->headb.next)->color = 4;
+		C_NODE(t_node, info->headb.prev)->color = 4;
 	}
 	info->nbope++;
 }
